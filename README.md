9dacc6f2c5b04f87e33befb9cba18b48a9c43500
Luciano Hayer <lucianohayer@gmail.com>
2021-09-10 00:17:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React_course_middlewares
+
+Simple aplicación para implementar middlewares dentro de React. Correspondiente al curso de "Advanced React and Redux - Udemy".
---
+++ src/components/CommentBox.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { saveComment, fetchComments } from 'actions';
+import { requireAuth } from 'components/requireAuth';
+
+class CommentBox extends Component {
+	state = { comment: '' };
+
+	handleChange = (event) => {
+		this.setState({ comment: event.target.value });
+	};
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+		this.props.saveComment(this.state.comment);
+		this.setState({ comment: '' });
+	};
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<h4>Add a comment</h4>
+					<textarea value={this.state.comment} onChange={this.handleChange} />
+					<div>
+						<button>Submit comment</button>
+					</div>
+				</form>
+				<button className="fetch-comments" onClick={this.props.fetchComments}>
+					Fetch comments
+				</button>
+			</div>
+		);
+	}
+}
+
+export default connect(null, { saveComment, fetchComments })(
+	requireAuth(CommentBox)
+);
